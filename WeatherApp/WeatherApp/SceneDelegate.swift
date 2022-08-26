af1b61a13450f9e672cf60433d868283b9b14ec1
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-26 18:59:45 +0900

    [DEL] Whitespaces, newlines

--- WeatherApp/WeatherApp/SceneDelegate.swift
+++ WeatherApp/WeatherApp/SceneDelegate.swift
@@ -11,21 +11,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
   func scene(
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
     options connectionOptions: UIScene.ConnectionOptions
   ) {
     guard let scene = scene as? UIWindowScene else { return }
-    
+
     let window = UIWindow(windowScene: scene)
     self.window = window
-    
+
     window.rootViewController = UINavigationController(rootViewController: ViewController())
-    
-    
+
     window.makeKeyAndVisible()
   }
 }
-
