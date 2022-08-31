feae5690a4181725b1f603e93043f7ebec591c5e
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-01 01:06:47 +0900

    [ADD] CityNameModel

---
+++ WeatherApp/WeatherApp/Source/Models/CityName.swift
@@ -0,0 +1,39 @@
+//
+//  CityName.swift
+//  WeatherApp
+//
+//  Created by 홍승현 on 2022/09/01.
+//
+
+import Foundation
+
+struct CityName: Codable {
+  let documents: [Document]
+  let meta: Meta
+}
+
+// MARK: - Document
+struct Document: Codable {
+  let addressName: String
+  let x: String
+  let y: String
+  
+  enum CodingKeys: String, CodingKey {
+    case addressName = "address_name"
+    case x
+    case y
+  }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+  let isEnd: Bool
+  let pageableCount: Int
+  let totalCount: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case isEnd = "is_end"
+    case pageableCount = "pageable_count"
+    case totalCount = "total_count"
+  }
+}
