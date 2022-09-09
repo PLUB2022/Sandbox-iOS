a82241721d5233be3801b73bcfffc9c9db3addb0
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-09 09:53:44 +0900

    [FEAT] 임시 uitableView

--- WeatherApp/WeatherApp/Source/Views/AddCity/SearchResultsViewController.swift
+++ WeatherApp/WeatherApp/Source/Views/AddCity/SearchResultsViewController.swift
@@ -7,11 +7,61 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class SearchResultsViewController: BaseViewController {
+
+  // MARK: - Properties
+
+  private lazy var tableView = UITableView().then {
+    $0.dataSource = self
+    $0.delegate = self
+    $0.register(UITableViewCell.self, forCellReuseIdentifier: "TableView")
+  }
 
   // MARK: - Life Cycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  // MARK: - Layout Configuration
+
+  override func setupLayouts() {
+    super.setupLayouts()
+    view.addSubview(tableView)
+  }
+
+  override func setupConstraints() {
+    super.setupConstraints()
+    tableView.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchResultsViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TableView", for: indexPath)
+    cell.textLabel?.text = "Hello, World!"
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SearchResultsViewController: UITableViewDelegate {
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let cell = tableView.cellForRow(at: indexPath)
+    dismiss(animated: true)
   }
 }
