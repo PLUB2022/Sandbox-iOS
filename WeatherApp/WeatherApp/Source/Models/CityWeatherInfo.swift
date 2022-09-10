e5f5f4a8d34721a5192a34733792943f3e819514
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-10 19:41:01 +0900

    [ADD] CityWeatherInfo

---
+++ WeatherApp/WeatherApp/Source/Models/CityWeatherInfo.swift
@@ -0,0 +1,12 @@
+//
+//  CityWeatherInfo.swift
+//  WeatherApp
+//
+//  Created by 홍승현 on 2022/09/10.
+//
+
+struct CityWeatherInfo: Codable {
+  let cityName: String
+  let temperature: Double
+  let weather: String
+}
